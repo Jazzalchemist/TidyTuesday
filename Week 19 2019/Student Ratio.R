f2419b01388a3191e6da2b07de47d82a0ac2847b
Jared Braggins <jared.braggins@hotmail.com>
2019-05-07 16:58:20 +1200

    Update Student Ratio.R

--- Week 19 2019/Student Ratio.R
+++ Week 19 2019/Student Ratio.R
@@ -1,10 +1,14 @@
 # Load packages
 library(tidyverse)
+library(rvest)
+library(magrittr)
+library(ggmap)
+library(stringr)
 
 # Import data
 student_ratio <- read.csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-05-07/student_teacher_ratio.csv")
 
-# View data
+# Inspect data
 View(student_ratio)
 
 # Group countries and calculate mean
@@ -12,3 +16,17 @@ ratio_grouped <-student_ratio %>%
   group_by(country) %>%
   mutate(mean = mean(student_ratio, na.rm = TRUE))
 
+ratio_grouped
+
+map.world <- map_data('world')
+
+map_data('world') %>% group_by(region) %>% summarise() %>% print(n = Inf)
+
+# Join datasets
+country_join <- left_join(map.world, ratio_grouped, by = c('region' = 'country'))
+
+View(country_join)
+
+map_data(country_join) %>%
+  ggplot(aes(x = long, y = lat, group = group)) +
+  geom_polygon(aes(fill = mean))
