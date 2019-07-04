e540a80de6392b18bc010d4d75003c6f11a896cc
Jared Braggins <jared.braggins@hotmail.com>
2019-07-05 09:27:36 +1200

    Update Media Franchise.R

--- Week 27 2019/Media Franchise.R
+++ Week 27 2019/Media Franchise.R
@@ -1,13 +1,10 @@
 # Load libraries
 library(tidyverse)
-library(ggfittext)
 library(extrafont)
 library(scales)
-library(grid)
-library(ggpubr)
 
 # Import data
-media_franchises <- read.csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-02/media_franchises.csv")
+media_franchises <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-07-02/media_franchises.csv")
 
 # Inspect data
 glimpse(media_franchises)
@@ -33,7 +30,7 @@ my_theme <- theme(text = element_text(family = my_font),
                   plot.background = element_rect(fill = my_background, color = NA),
                   plot.title = element_text(face = 'bold', size = 22, colour = my_titlecolour),
                   plot.subtitle = element_text(size = 16, colour = my_textcolour),
-                  plot.caption = element_text(size = 14, colour = my_textcolour),
+                  plot.caption = element_text(size = 12, colour = my_textcolour),
                   panel.background = element_rect(fill = my_background, color = NA),
                   panel.border = element_blank(),
                   panel.grid.major.y = element_blank(),
@@ -43,7 +40,8 @@ my_theme <- theme(text = element_text(family = my_font),
                   axis.title = element_blank(),
                   axis.text.y = element_text(size = 12, colour= my_textcolour),
                   axis.text.x = element_text(size = 12, colour = my_textcolour),
-                  axis.ticks = element_blank(),
+                  axis.ticks.y = element_blank(),
+                  axis.ticks.x = element_line(size = 0.5, colour = my_textcolour),
                   axis.line.x = element_line(colour = "white", size = 0.5, linetype = "solid"),
                   legend.position="none")
 
@@ -60,9 +58,11 @@ top5 %>%
   annotation_custom(rast, ymin = 40, ymax = 60, xmin = -2) +
   geom_segment(aes(y = 0, xend = franchise, yend = total_revenue), colour = "grey50") +
   geom_point(aes(colour = highlight_flag), size = 8) +
-  scale_fill_manual(values = c('grey65', 'skyblue4')) +
+  scale_colour_manual(values = c(my_textcolour, my_titlecolour)) +
   scale_y_continuous(labels = scales::dollar_format(prefix="$", suffix = "bn")) +
   coord_flip() +
   labs(title = "Star Wars For the Win",
-       subtitle = "Based on the Top Five Film Franchises by Revenue (Billions)",
+       subtitle = "Based on the Top Five Film Franchises by Revenue",
        caption = "\nVisualisation: @JaredBraggins | Data Source: Wikipedia")
+
+ggsave("media_franchise.png", width = 10, height = 6)
